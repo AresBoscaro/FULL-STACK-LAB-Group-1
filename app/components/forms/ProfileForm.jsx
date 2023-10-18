74cd7ecaf43acd225b8febd17d18d596bc78a3a8
moundeveloper <moundeveloper@gmail.com>
2023-10-18 15:08:40 +0200

    Update-profile-working

--- app/components/forms/ProfileForm.jsx
+++ app/components/forms/ProfileForm.jsx
@@ -3,35 +3,34 @@
 
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useUser } from "@/app/context/user-provider";
 
 export default function ProfileForm() {
   const supabase = createClientComponentClient();
 
-  const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [password, setPassword] = useState("");
-  const [matricola, setMatricola] = useState("");
+  const { Profile } = useUser();
+  const [name, setName] = useState(Profile.first_name);
+  const [lastname, setLastname] = useState(Profile.last_name);
+  const [matricola, setMatricola] = useState(Profile.stud_id);
 
   const inputs = [
     {
       label: "Name",
       type: "name",
       name: "name",
+      value: name,
     },
     {
       label: "Lastname",
       type: "lastname",
       name: "lastname",
-    },
-    {
-      label: "Password",
-      type: "password",
-      name: "password",
+      value: lastname,
     },
     {
       label: "Matricola",
       type: "matricola",
       name: "matricola",
+      value: matricola,
     },
   ];
 
@@ -55,18 +54,32 @@ export default function ProfileForm() {
     }
   };
 
-  async function updateUser(userId, updatedUserData) {
+  async function updateUser() {
+    const updatedUserData = {};
+
+    if (name !== Profile.first_name) {
+      updatedUserData.first_name = name;
+    }
+    if (lastname !== Profile.last_name) {
+      updatedUserData.last_name = lastname;
+    }
+    if (matricola !== Profile.stud_id) {
+      updatedUserData.stud_id = matricola;
+    }
+
+    console.log(updatedUserData);
+
     const { data, error } = await supabase
       .from("profiles")
       .update(updatedUserData)
-      .eq("id", userId);
+      .eq("id", Profile.id);
 
     if (error) {
       console.error("Error updating user:", error);
       return null; // Handle the error as needed
     }
 
-    return data; // The updated user data
+    return data;
   }
 
   return (
@@ -80,22 +93,23 @@ export default function ProfileForm() {
             <input
               name={input.name}
               type={input.type}
-              onChange={(e) => handleInputChange}
+              onChange={handleInputChange}
               className="w-full p-2 rounded-lg border-[1px] border-slate-900/40 outline-none"
+              value={input.value}
             />
           </div>
         ))}
       </div>
-      <ConfirmButton />
+      <ConfirmButton onClick={updateUser} />
     </div>
   );
 }
 
-function ConfirmButton() {
+function ConfirmButton({ onClick }) {
   return (
     <button
       className="w-full rounded-xl p-2 text-center bg-slate-900"
-      onClick={() => updateUser()}
+      onClick={onClick}
     >
       <h3 className="text-white font-semibold text-lg">confirm</h3>
     </button>
